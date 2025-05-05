2ef6520de9b61e8e1fea02fb4d969f83c1747208
stef.ilic <stef.ilic@protonmail.com>
2025-05-06 01:06:37 +0200

    feat: display qr reader result

--- app/src/util/qrcodeprocessor/controladdin/qrcodescanner/scripts/qrcodescanner.js
+++ app/src/util/qrcodeprocessor/controladdin/qrcodescanner/scripts/qrcodescanner.js
@@ -11,39 +11,37 @@ function Initiliaze() {
     const canvas = document.createElement('canvas');
     canvas.id = "stil-canvas";
     canvas.width = 400;
-    canvas.height = 300;
+    canvas.height = 220;
     videoContainer.appendChild(canvas);
 
-        video.onpause = function() {
-          clearInterval(canvasInterval);
-        };
-        video.onended = function() {
-          clearInterval(canvasInterval);
-        };
-        video.onplay = function() {
-          clearInterval(canvasInterval);
-          canvasInterval = window.setInterval(() => {
-           processFrame();
-          }, 1000 / 60);
-        };
+    video.onpause = function() {
+      clearInterval(canvasInterval);
+    };
+    video.onended = function() {
+      clearInterval(canvasInterval);
+    };
+    video.onplay = function() {
+      clearInterval(canvasInterval);
+      canvasInterval = window.setInterval(() => {
+        processFrame();
+      }, 1000 / 60);
+    };
 }
 
 function processFrame () {
-  // debugger;
   const videoContainer = window.parent.document.querySelector(".react-html5-camera-photo");
   const video = videoContainer.querySelector("video");
   if (video === null) return;
   const canvas = videoContainer.querySelector("#stil-canvas");
-  const ctx = canvas.getContext("2d", { alpha: false });
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-  // const code = await readBarcodeFromCanvas(canvas);
-  // if (code.format) {
-  //   alert(code.format + ": " + escapeTags(code.text));
-  //   drawResult(code, ctx);
-  // } else {
-  //   console.log("No barcode found");
-  // }
-  requestAnimationFrame(processFrame());
+  if (canvas === null) return;
+  const ctx = canvas.getContext("2d", { willReadFrequently: true });
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  readBarcodeFromCanvas(canvas).then(result => {
+    if (result.format) {
+      alert(result.format + ": " + escapeTags(result.text));
+      drawResult(result, ctx);
+    }
+  });
 }
 
 function drawResult(code, ctx) {
@@ -60,29 +58,22 @@ function drawResult(code, ctx) {
   }
 }
 
-async function readBarcodeFromCanvas(canvas) {
-  debugger;
-  var zxing = ZXing().then(function (instance) {
-    zxing = instance; // this line is supposedly not required but with current emsdk it is :-/
-  });
-  // const bitmap = await createImageBitmap(video);
-  return;
-  var _malloc = zxing._malloc;
-
+function readBarcodeFromCanvas(canvas) {
   var imgWidth = canvas.width;
   var imgHeight = canvas.height;
-  var imageData = canvas.getContext('2d').getImageData(0, 0, imgWidth, imgHeight);
+  var imageData = canvas.getContext("2d", { willReadFrequently: true }).getImageData(0, 0, imgWidth, imgHeight);
   var sourceBuffer = imageData.data;
-
-  if (zxing != null) {
-    var buffer = zxing._malloc(sourceBuffer.byteLength);
-		zxing.HEAPU8.set(sourceBuffer, buffer);
-		var result = zxing.readBarcodeFromPixmap(buffer, imgWidth, imgHeight, true, "");
-		zxing._free(buffer);
-    return result;
-  } else {
-    return { error: "ZXing not yet initialized" };
-  }
+  return ZXing().then(function (zxing) {
+    if (zxing) {
+      var buffer = zxing._malloc(sourceBuffer.byteLength);
+      zxing.HEAPU8.set(sourceBuffer, buffer);
+      var result = zxing.readBarcodeFromPixmap(buffer, imgWidth, imgHeight, true, "");
+      zxing._free(buffer);
+      return result;
+    } else {
+      return { error: "ZXing not yet initialized" };
+    }
+  });
 }
 
 function escapeTags(htmlStr) {
--- app/src/util/qrcodeprocessor/interface/QRCodeScanner.Interface.al
+++
@@ -1,9 +0,0 @@
-namespace STIL.Util.QRCodeProcessor.Interfaces;
-
-interface "STI QR Code Scanner"
-{
-    Access = Internal;
-
-    procedure ScanQRCode(Args: JsonObject; var Result: JsonObject);
-    procedure Initiliaze();
-}
